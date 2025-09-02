cc3b8fea52ef0e993798564b8cf854652433c939
hassanzaheer88 <sh.hassan607@gmail.com>
2025-09-03 02:02:05 +0500

    create a middleware and fn to handle error manually

--- api/index.js
+++ api/index.js
@@ -24,4 +24,14 @@ app.listen(3000 ,() => {
 app.use('/api/user' , userRouter)
 app.use('/api/auth' , authRouter)
 
+app.use((err,req,res,next) => {
+    const statuscode = err.statuscode || 500
+    const message = err.message || 'Internal server error'
+    return res.status(statuscode).json({
+        success: false,
+        statuscode,
+        message,
+    })
+})
+
 
