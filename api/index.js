da3b08936ae774461fb8609c67556665c9da0027
hassanzaheer88 <sh.hassan607@gmail.com>
2026-04-11 03:54:32 +0500

    complete signup page

--- api/index.js
+++ api/index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import { error, log } from "console";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 dotenv.config();
